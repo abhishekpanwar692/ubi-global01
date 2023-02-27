de42c0ebd26fdb59ed9229d830c178de8df22b86
Abhishek <abhishekpanwar692@gmail.com>
2023-02-27 11:20:35 +0530

    this is globel

---
+++ script.js
@@ -0,0 +1,10 @@
+
+  function myfunction(){
+    var nav=document.querySelector(".Nav-Bar");
+     nav.style.display='flex'
+  }
+  function myclick(){
+    var nav=document.querySelector(".Nav-Bar");
+     nav.style.display='none'
+  }
+
